565257a4f90203971cc54c8d95389c485d0ebfb9
petrospe <petros_pe@yahoo.gr>
2021-09-10 23:08:50 +0300

    show content articles and menu

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,7 @@ let content;
 let contentURL = 'json/content.json';
 
 const articles = document.getElementById("articles");
+const articleList = document.getElementById("articleList");
 
 fetch(contentURL)
 .then(response => response.json())
@@ -39,34 +40,74 @@ fetch(contentURL)
       divcard.classList.add("first");
 
     var titleH2 = document.createElement('h2');
-    var titleA = document.createElement('a');
-    titleA.setAttribute("href", "javascript: void(0)");
-    var titleH2Text = document.createTextNode(content.articles[key].title);
-    titleA.appendChild(titleH2Text);
-    titleH2.appendChild(titleA);
+    titleH2.innerHTML = `<a href="javascript: void(0)" onclick="show(`+key+`)">`+content.articles[key].title+`</a>`;
     divcard.appendChild(titleH2);
+
+    if(key==0)
+      var ul = document.createElement('ul');
+      ul.classList.add("dropdown");
+      articleList.appendChild(ul);
+
+    var li = document.createElement('li');
+    li.innerHTML = `<a href="javascript: void(0)" onclick="show(`+key+`)">`+content.articles[key].title+`</a>`;
+    ul.appendChild(li);
 
     var dateP = document.createElement('p');
     dateP.classList.add("date");
-    var datePText = document.createTextNode(content.articles[key].date);
-    dateP.appendChild(datePText);
+    dateP.innerHTML = content.articles[key].date;
     divcard.appendChild(dateP);
 
     var teaser = content.articles[key].text.split(' ').slice(0, 39).join(' ');
+    var teaserP = document.createElement('p');
+    teaserP.classList.add("text");
+    teaserP.classList.add("teaser");
+    teaserP.innerHTML = teaser+" ...";
+    divcard.appendChild(teaserP);
+
     var textP = document.createElement('p');
     textP.classList.add("text");
-    var textPText = document.createTextNode(teaser);
-    textP.appendChild(textPText);
+    textP.classList.add("hide");
+    textP.innerHTML = content.articles[key].text;
     divcard.appendChild(textP);
 
-    var buttonText = document.createTextNode('Show more');
     var buttonShowMore = document.createElement('button');
     buttonShowMore.classList.add("button");
     buttonShowMore.classList.add("button-clear");
-    buttonShowMore.appendChild(buttonText);
+    buttonShowMore.setAttribute("onclick", "show("+key+")");
+    buttonShowMore.innerHTML = 'Show more';
     divcard.appendChild(buttonShowMore);
 
     articles.appendChild(divcard);
-    
   }
 })
+
+function show(id){
+  var article = document.getElementById("article-"+id);
+  article.classList.remove("hide");
+  article.classList.add("first");
+
+  var teaser = article.getElementsByClassName('teaser');
+  if(teaser.length)
+    teaser[0].remove();
+
+  var text = article.getElementsByClassName('text');
+  text[0].classList.remove("hide");
+
+  var button = article.getElementsByClassName('button');
+  if(button.length)
+    button[0].remove();
+
+  var buttonClose = document.createElement('button');
+  buttonClose.classList.add("button");
+  buttonClose.classList.add("button-clear");
+  buttonClose.setAttribute("onclick", "location.reload();");
+  buttonClose.innerHTML = 'Close';
+  article.appendChild(buttonClose);
+
+  var otherArticles = document.querySelectorAll(".card:not(#article-"+id+")");
+  for (const [key, otherArticle] of Object.entries(otherArticles)) {
+      // otherArticle.remove();
+      otherArticle.classList.add("hide");
+      otherArticle.classList.remove("first");
+  }
+}
